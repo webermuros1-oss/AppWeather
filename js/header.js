e8d70a51ac9ddeb03477d06d8e873743e09f2263
webermuros1 <webermuros1@gmail.com>
2026-01-09 13:14:31 +0100

    feat:create header html and js

---
+++ js/header.js
@@ -0,0 +1,12 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+
+    <header class="appHeader">
+        <h1 class="title"><img class="weatherIcon" src="/images/logoWeatherPlus.png" alt="weather logo"></h1>
+		<input id="getCity" type="text" placeholder="Busca ciudad aquí" />
+    </header>
+    `;
+    }
+}
+customElements.define("header-plus-weather", MyHeader);
