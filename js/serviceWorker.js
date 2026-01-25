35749ad6b1ff23e93a888bbea924defee9e3894d
webermuros1 <webermuros1@gmail.com>
2026-01-25 16:46:58 +0100

    patch:clean code and routes de serviceworker

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,16 +1,35 @@
-const CACHE_NAME = "meteo-cache-v1";
+const CACHE_NAME = "meteo-cache-v2";
+
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./css/index.css",
+    "./css/header.css",
+    "./css/footer.css",
+    "./js/index.js",
+    "./js/header.js",
+    "./js/footer.js",
+    "./media/images/logoRemaster192.png",
+    "./media/images/logoRemaster512.png"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
